e2c67d9182c6751bae7ce12514de0875cb3d5a01
Mona Lisa <77768569+Archi2903@users.noreply.github.com>
2021-07-19 21:43:24 +0300

    #9 function edited,save(finished). admin:edit category

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ Route::resource('rest','RestController')->names('rest');
 Route::group(['namespace'=>'Blog','prefix'=>'blog'],function (){
    Route::resource('posts','PostController')->names('blog.posts');
 });
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
